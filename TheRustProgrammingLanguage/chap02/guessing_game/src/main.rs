45e0c410b77c7dc651712c22680a43af9029a1f3
feixiao <feixiao2020@sina.com>
2021-10-12 10:51:08 +0800

    处理非法输入

--- TheRustProgrammingLanguage/chap02/guessing_game/src/main.rs
+++ TheRustProgrammingLanguage/chap02/guessing_game/src/main.rs
@@ -27,8 +27,16 @@ fn main() {
 
         // Rust允许使用同名的新变量guess来隐藏 （shadow）旧变量的值。
         // 新创建的guess变量被绑定到了表达式guess.trim().parse()产生的结果上。
-        let guess: u32 = guess.trim().parse()
-            .expect("Please type a number!");
+        // let guess: u32 = guess.trim().parse()
+        //     .expect("Please type a number!");
+
+        // 输入错误版本
+        // 我们使用了match表达式来替换之前的expect方法，这是我们处理错误行为的一种惯用手段。
+        // parse会返回一个Result类型，而Result则包含了Ok与Err两个变体。
+        let guess: u32= match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
 
         // match表达式由数个分支 （arm）组成，每个分支都包含一个用于匹配的模式 （pattern），以及匹配成功后要执行的相应的代码。
         // Rust会尝试用我们传入match表达式的值去依次匹配每个分支的模式，一旦匹配成功，它就会执行当前分支中的代码。
