a75c8cfbd884b1676eb49b14fb06ad58686dba2d
feixiao <feixiao2020@sina.com>
2021-10-13 00:20:00 +0800

    update

--- TheRustProgrammingLanguage/chap04/ownership/src/main.rs
+++ TheRustProgrammingLanguage/chap04/ownership/src/main.rs
@@ -13,6 +13,13 @@ fn main() {
     // 变量x同样被传递到函数，但是由于i32的Copy的，所以后面可以继续使用x
     makes_copy(x);
 
+
+    let s1 = gives_ownership();  // 返回值移到到了s1
+
+    let s2 = String::from("hello");
+
+    // s2被移动到函数内部，返回值又被移到变量s3
+    let s3 = takes_and_gives_back(s2);
 }
 
 
@@ -22,4 +29,17 @@ fn take_ownership(some_thing: String) {
 
 fn makes_copy(some_integer: i32) {
     println!("{}", some_integer);
+}
+
+// gives_ownership 会将它的返回值移到调用它的函数内
+fn gives_ownership() -> String {
+    let some_string = String::from("hello");
+
+    some_string  // 作为返回值移到了
+
+}
+
+
+fn takes_and_gives_back(a_string : String) -> String {
+    a_string    // 作为返回值移到调用函数
 }
