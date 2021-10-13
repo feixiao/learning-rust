6e11b28afaffbfb9b3df7ad39b4e9738ffcbf7ef
feixiao <feixiao2020@sina.com>
2021-10-13 13:50:09 +0800

    vector使用

---
+++ TheRustProgrammingLanguage/chap08/vector_example/src/main.rs
@@ -0,0 +1,37 @@
+fn main() {
+   // 创建一个用来存储i32数据的空动态数组
+   let v:Vec<i32> = Vec::new();
+   println!("vector {:?}", v);
+
+
+   // Rust特意提供了一个用于简化代码的vec!
+   // 这个宏可以根据我们提供的值来创建一个新的动态数组。
+   let v = vec![1, 2, 3];
+   println!("vector {:?}", v);
+
+
+   // 使用push方法将值添加到动态数组中
+   let mut v = Vec::new();
+   v.push(5);
+   v.push(6);
+   v.push(7);
+   v.push(8);
+   println!("vector {:?}", v);
+
+
+   // 访问动态数组的方式，它们分别是使用索引和get方法
+   let v = vec![1, 2, 3, 4, 5];
+   let third: &i32 = &v[2];   // 使用索引方式
+   println!("The third element is {}", third);
+   match v.get(2) {  // get方法,会返回一个Option<&T>。
+      Some(third) => println!("The third element is {}", third),
+      None => println!("There is no third element."),
+   }
+
+
+   // 遍历动态数组中的值
+   let v = vec![100, 32, 57];
+   for i in &v {
+      println!("{}", i);
+   }
+}
