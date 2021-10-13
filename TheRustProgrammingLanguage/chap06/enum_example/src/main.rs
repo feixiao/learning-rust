972a6f55f92a65d63b4237476c6e412b62d7dc81
feixiao <feixiao2020@sina.com>
2021-10-13 10:47:45 +0800

    enum使用

---
+++ TheRustProgrammingLanguage/chap06/enum_example/src/main.rs
@@ -0,0 +1,19 @@
+#[derive(Debug)]
+enum IpAddrKind {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+#[derive(Debug)]
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String,
+}
+
+fn main() {
+    let home = IpAddrKind::V4(127, 0, 0, 1);
+    let loopback = IpAddrKind::V6(String::from("::1"));
+
+    println!("home is {:?}", home);
+    println!("loopback is {:?}", loopback);
+}
