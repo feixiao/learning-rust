2d2179112dfc30fb448afa379752f343b44df596
feixiao <feixiao2020@sina.com>
2021-10-13 09:56:33 +0800

    update

--- TheRustProgrammingLanguage/chap05/structs/src/main.rs
+++ TheRustProgrammingLanguage/chap05/structs/src/main.rs
@@ -1,3 +1,28 @@
+
+
+// 结构体的定义
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+// build_user函数中使用了相同的参数名与字段名，并采用了字段初始化简写语法进行编写
+fn build_user(email: String, username: String) -> User {
+    User {
+        email,   // 因为参数名与字段名相同所以省略
+        username,
+        active: true,
+        sign_in_count: 1,
+    }
+}
+
+
+// 定义元组结构体时依然使用struct关键字开头，并由结构体名称及元组中的类型定义组成
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+
 fn main() {
     // 不可变结构
     let user = User {
@@ -27,23 +52,13 @@ fn main() {
         username: String::from("anotherusername567"),
         ..user1     // 使用结构体更新语法来为一个User实例设置新的email和username字段的值，并从user1实例中获取剩余字段的值
     };
+
+
+    // 这里的black和origin是不同的类型，因为它们两个分别是不同元组结构体的实例。
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
 }
 
 
 
-// 结构体的定义
-struct User {
-    username: String,
-    email: String,
-    sign_in_count: u64,
-    active: bool,
-}
-// build_user函数中使用了相同的参数名与字段名，并采用了字段初始化简写语法进行编写
-fn build_user(email: String, username: String) -> User {
-    User {
-        email,   // 因为参数名与字段名相同所以省略
-        username,
-        active: true,
-        sign_in_count: 1,
-    }
-}
