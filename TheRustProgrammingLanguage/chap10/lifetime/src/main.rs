504729b64cd9b86f7f69d5a0312728ea306842b9
feixiao <feixiao2020@sina.com>
2021-10-14 13:20:10 +0800

    理解生命周期

--- TheRustProgrammingLanguage/chap10/lifetime/src/main.rs
+++ TheRustProgrammingLanguage/chap10/lifetime/src/main.rs
@@ -12,6 +12,7 @@
 
 // 生命周期的标注并不会改变任何引用的生命周期长度。
 // 在这个签名中我们所表达的意思是：参数与返回值中的所有引用都必须拥有相同的生命周期。
+// 未来，需要手动标注的生命周期也许会越来越少
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
@@ -21,6 +22,14 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 }
 
 
+// <'a> 这个标注意味着ImportantExcerpt实例的存活时间不
+// 能超过存储在part字段中的引用的存活时间。
+struct ImportantExcerpt<'a> {
+    part: &'a str,      // 用于存储一个字符串切片，也就是一个引用
+}
+
+
+
 fn main() {
     let string1 = String::from("long string is long");
     {
@@ -28,4 +37,13 @@ fn main() {
         let result = longest(string1.as_str(), string2.as_str());
         println!("The longest string is {}", result);
     }
+
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+
+    let first_sentence = novel.split('.')
+      .next()
+      .expect("Could not find a '.'");
+
+    let i = ImportantExcerpt { part: first_sentence };
 }
