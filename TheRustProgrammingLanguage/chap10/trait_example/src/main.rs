fa621f49e28334bdc18e6bc0caf844b9c41a4769
feixiao <feixiao2020@sina.com>
2021-10-14 10:32:34 +0800

    update

--- TheRustProgrammingLanguage/chap10/trait_example/src/main.rs
+++ TheRustProgrammingLanguage/chap10/trait_example/src/main.rs
@@ -21,6 +21,18 @@ impl Summary for NewsArticle {
     }
 }
 
+// 使用trait作为参数, 语法糖 等价下面的函数
+// pub fn notify(item: impl Summary + Display)  通过+语法来指定多个trait约束
+pub fn notify(item: impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+
+// pub fn notify<T: Summary>(item: T) {
+//     println!("Breaking news! {}", item.summarize());
+// }
+
+
+
 fn main() {
 
     let article = NewsArticle {
@@ -30,4 +42,6 @@ fn main() {
         content: String::from("The Pittsburgh Penguins once again are the best hockey team in the NHL."),
     };
     println!("New article available! {}", article.summarize());
+
+    notify(article);
 }
