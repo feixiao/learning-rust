49b244e3449c181a95b79b4e20c27bef2b2981c2
feixiao <feixiao2020@sina.com>
2021-10-15 09:29:19 +0800

    熟悉智能指针

--- TheRustProgrammingLanguage/chap15/box_example/src/main.rs
+++ TheRustProgrammingLanguage/chap15/box_example/src/main.rs
@@ -1,5 +1,5 @@
 
-
+use std::ops::Deref;
 use crate::List::{Cons, Nil};
 
 enum List {
@@ -8,6 +8,29 @@ enum List {
 }
 
 
+
+// 定义了一个名为MyBox的结构体。结构体的定义中附带了泛型参数T，因为我们希望它能够存储任意类型的值。
+struct  MyBox<T>(T);
+
+impl<T> MyBox<T>  {
+    fn new(x:T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+
+
+// 通过实现Deref trait来将类型视作引用
+impl<T> Deref for MyBox<T> {
+    type Target = T;        // 定义了Deref trait的一个关联类型
+    fn deref(&self) -> &T {
+         &self.0
+    }
+}
+
+
+fn hello(name: &str) {
+    println!("Hello, {}!", name);
+}
 
 fn main() {
 
@@ -20,4 +43,30 @@ fn main() {
                     Box::new(Cons(2,
                                   Box::new(Cons(3,
                                                 Box::new(Nil))))));
+
+
+    let x = 5;
+
+    let y = &x;
+    // 把Box<T>当成引用来操作
+    let z = Box::new(x);
+
+
+    assert_eq!(5,x);
+    assert_eq!(5,*y);
+    assert_eq!(5,*z);
+
+
+    let y = MyBox::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+
+    // 解引用转换特性使我们可以将MyBox<String>值的引用传入hello函数
+    // 解引用转换（deref coercion）是Rust为函数和方法的参数提供的一种便捷特性。
+    // 当某个类型T实现了Deref trait时，它能够将T的引用转换为T经过Deref操作后生成的引用。
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
+    // 如果Rust没有解引用转换功能，就必须编写这样的代码
+    // hello(&(*m)[..]);
 }
